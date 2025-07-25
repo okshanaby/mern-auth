397730ec14557ead06f038beac369786f98182e4
OkkashaAlly <steamnature006@gmail.com>
2025-07-25 22:29:47 +0300

    logout imlemented

---
+++ client/src/components/logout-btn.tsx
@@ -0,0 +1,35 @@
+import API from "@/api/axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import { Button } from "./ui/button";
+
+const LogoutBtn = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    setIsLoading(true);
+    try {
+      const res = await API.get("/auth/logout");
+      if (res.data.success) {
+        toast.success(res.data.message);
+        navigate("/login");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error: any) {
+      console.log("🚀 ~ handleLogout ~ error:", error);
+      toast.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <Button type="submit" onClick={handleLogout} disabled={isLoading}>
+      {isLoading ? "Loading..." : "Log out"}
+    </Button>
+  );
+};
+
+export default LogoutBtn;
--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -1,9 +1,13 @@
 import API from "@/api/axios";
+import LogoutBtn from "@/components/logout-btn";
+import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const DashboardPage = () => {
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState<any>(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getUserProfile = async () => {
@@ -28,9 +32,17 @@ const DashboardPage = () => {
   if (loading) return <div>Loading...</div>;
 
   return (
-    <div>
+    <div className="p-48 space-y-10">
       <h1>Dashboard</h1>
       <div className="">{JSON.stringify(user)}</div>
+      <div className="flex gap-3">
+        {!user.isAccountVerified ? (
+          <Button onClick={() => navigate("/verify-account")}>
+            Verify Account
+          </Button>
+        ) : null}
+        <LogoutBtn />
+      </div>
     </div>
   );
 };
